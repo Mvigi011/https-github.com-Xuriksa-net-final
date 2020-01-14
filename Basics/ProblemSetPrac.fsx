9d75da1fa85f2b2e6c7760cd3bac8e950d8fd504
Michael V's PC <Michael V's PC@LAPTOP-G17J4HIR>
2020-01-14 11:38:16 -0500

    In the main of Basics, I tried problem 22 in a script file but kept getting small but annoying errors so I tried it in the main, but for some reason the reverseLists is not printing out and I tried 2 other ways to print but cant get it to work

---
+++ Basics/ProblemSetPrac.fsx
@@ -0,0 +1,14 @@
+﻿
+module ProblemSetPrac
+
+// practice 1 set problems and potential solutions
+// PROBLEM 5 Which of the following F# expressions evaluates to [1; 2; 3]? Select one:
+ [1; 2; 3]
+ // accoding to F# interactive it gives a val int: [1,2,3]
+ 
+ //problem 22 
+ let lists = [[0;1;1];[3;2];[];[5]]
+ let reverseLists list = list |> List.map List.rev  
+ printfn "%A" reverseLists;
+ // PROBLEM 2 
+ 
--- Basics/Program.fs
+++ Basics/Program.fs
@@ -6,4 +6,8 @@ open System
 let main argv =
 
     printfn "Hello World from F#!";
+    let lists = [[0;1;1];[3;2];[];[5]]
+    let revList list = list |> List.map List.rev  
+  //  printfn "%A" revList
+  //  printfn "%d" revList
     0 // return an integer exit code
