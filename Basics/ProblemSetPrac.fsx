099692c10a02fa0df7aa8302358604378b59facb
Michael V's PC <Michael V's PC@LAPTOP-G17J4HIR>
2020-01-15 15:55:28 -0500

    In this one I tried to follow a tutorial on list and printing them out, I tried using a script file and the main to run the code, but for some reason I can get rid of the squiggle line for the "ignore" command. I tried playing aruond with indents, typing or deleting certain pieces of code in the beginning and end of the black of code to maybe find a possible solution. Then I looked online but couldn't really find a solid solution. So I am still working just trying to print out a list

--- Basics/ProblemSetPrac.fsx
+++ Basics/ProblemSetPrac.fsx
@@ -1,14 +1,34 @@
 ﻿
 module ProblemSetPrac
 
+open System
+
+//open System
 // practice 1 set problems and potential solutions
 // PROBLEM 5 Which of the following F# expressions evaluates to [1; 2; 3]? Select one:
- [1; 2; 3]
+// [1; 2; 3]
  // accoding to F# interactive it gives a val int: [1,2,3]
  
  //problem 22 
- let lists = [[0;1;1];[3;2];[];[5]]
- let reverseLists list = list |> List.map List.rev  
- printfn "%A" reverseLists;
+ //let lists = [[0;1;1];[3;2];[];[5]]
+// let reverseLists list = list |> List.map List.rev  
+// printfn "%A" reverseLists;
+ // tutorial for list practice
+ (*let list_items() =
+     let list1 = [1;2;3;4]
+     list1 |> List.iter (printfn "Num : %i")
+     printfn "%A" list1
+
+     let list2 = 5::6::7::[]
+     printfn "%A" list2 
+
+     let list3 = [1..5]
+     let list4 =['a'..'g']
+     printfn "%A" list4
+
+     let list5 = List.init 5 (fun i -> i * 2)
+     printfn "%A" list5
+ list_items()
+ Console.ReadKey() |> ignore *) 
  // PROBLEM 2 
  
--- Basics/Program.fs
+++ Basics/Program.fs
@@ -1,13 +1,35 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
-
+//
 [<EntryPoint>]
 let main argv =
 
-    printfn "Hello World from F#!";
+   // printfn "Hello World from F#!";
     let lists = [[0;1;1];[3;2];[];[5]]
-    let revList list = list |> List.map List.rev  
-  //  printfn "%A" revList
+
+    let revList list = list |> List.map List.rev 
+   // revList |> List.iter (printfn "%i")
+    printfn "%A" revList 
+
+
   //  printfn "%d" revList
-    0 // return an integer exit code
+
+  // f# tutorial on List
+   (* let list_items() =
+      let list1 = [1;2;3;4]
+      list1 |> List.iter (printfn "Num : %i")
+      printfn "%A" list1
+
+      let list2 = 5::6::7::[]
+      printfn "%A" list2 
+
+      let list3 = [1..5]
+      let list4 =['a'..'g']
+      printfn "%A" list4
+
+      let list5 = List.init 5 (fun i -> i * 2)
+      printfn "%A" list5
+    list_items()
+    Console.ReadKey() |> ignore *)
+
+0// return an integer exit code
