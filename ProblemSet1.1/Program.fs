db414f372ba5e81cc88e4c7d0fb6dae3b7f94b61
Michael V's PC <Michael V's PC@LAPTOP-G17J4HIR>
2020-01-26 18:37:22 -0500

    These next few difficult probelms proved challenging becuase it required some additional think time, but even for some I couldn't answer. Also, some are logically sound but just cant run correctly so I will leave it at this.

--- ProblemSet1.1/Program.fs
+++ ProblemSet1.1/Program.fs
@@ -5,24 +5,82 @@ open System
 // problem 20 
 // this next problem is shuffling to get back to the original number
 
-let perfectShuffle xs =
+let shuffleCards xs =
  let h = (List.length xs) / 2
  xs
  |> List.mapi (fun i x->(if i<h then i * 2 else ((i-h) * 2) + 1), x)
  |> List.sortBy fst
  |> List.map snd
 
-let orderCount n =
+let Count1 n =
  let xs = [1..n]
  let rec spin count ys =
    if xs=ys then count
-   else ys |> perfectShuffle |> spin (count + 1)
- xs |> perfectShuffle |> spin 1
+   else ys |> shuffleCards |> spin (count + 1)
+ xs |> shuffleCards |> spin 1
 
-[ 8; 24; 52] |> List.iter (fun n->n |> orderCount |> printfn "input : %d ----> output %d" n)
+[ 8; 24; 52] |> List.iter (fun n->n |> Count1 |> printfn "input : %d ----> output %d" n)
 
 // problem 21 uncurried function with cartesian product
-let rec cartesian = function
-    | (xs, []) -> []
-    | (xs, y::ys) -> List.map (fun x -> (x, y)) xs @ cartesian (xs, ys) ;;
-printfn " the cartesian product is %A" cartesian(["a"; "b"; "c"] [1; 2])
+//skip
+
+//Problem 22
+let rec powerSet =
+   //Start the definition of the function.
+   function
+ //If the list is empty then return a list containing an empty list..
+   | [] -> [[]]
+   //Otherwise, Break the list into the subsets and call
+   //the function recursively to find all the possible subsets.
+   | (h::t) ->
+      let ht = powerSet t
+      List.map (fun t' -> h::t') ht @ ht
+//Call the function powerset to obtain the powerset of a list and
+//store the resultant powerset in a variable.
+let printPowSet = powerSet [1;2;3]
+//Print the powerset.
+printfn "%A" printPowSet;;
+
+// Problem 23
+//skip
+// Problem 24 sorting
+(*let swap x y (arr : 'a []) =
+
+    let temp1 = arr.[x]
+
+    arr.[x] <- arr.[y]
+
+    arr.[y] <- temp1
+
+let bubbleSortArray arr =
+
+    let rec loop (arr : 'a []) =
+
+        let mutable swaps = 0
+
+        for i = 0 to arr.Length - 2 do
+
+         if arr.[i] > arr.[i+1] then
+
+            swap i (i+1) arr
+
+        swaps <- swaps + 1
+
+        if swaps > 0 then loop arr else arr
+
+    loop arr
+    
+let bubbleSortArr = bubbleSortArray[3;1;4;1;5;9;2;6;5]
+printfn "%A" bubbleSortArr *)
+
+// pronblem 24
+(* cant figure out*)
+// problem 25
+(* 
+E -> E+T | E-T | T
+
+// T -> T*P | T/P | P
+
+P -> F^P | F
+
+F -> i | (E) *)
