fd9881df9996af26a9048b1724808c059c715211
Michael V's PC <Michael V's PC@LAPTOP-G17J4HIR>
2020-01-26 00:50:10 -0500

    Here is some of the hard codeing problems some have second attemps but Just cant get it to work. Also I got the reverse list problem correct, but some reason it prints the reverse first before the original list, even though the original print statement is on top of the reverse so its supposed to have precedence but doesnt?

--- ProblemSet/Program.fs
+++ ProblemSet/Program.fs
@@ -5,16 +5,16 @@ open System
 
 // this is my first approach but obiviously didnt work so had to think of
 // a more long but only way to do it simple. 
- (* let (.+) (a,b) (c,d) = ((a*c)/(a*d))+((b*c)/(b*d))
+(*let (.+) (a,b) (c,d) = ((a*c)/(a*d))+((b*c)/(b*d))
 
-let (.+) (1,2)(1,3) *)
+let (.+) (1,2)(1,3)  *)
 
 
 //let (.+) (a,b)(c,d)
 //let a,b,c,d = 1.0,2.0,2.0,3.0
 // this is the second attempt but just keep ending up with 7 so 
 
-let (.+) (a,b)(c,d) = 
+(*let (.+) (a,b)(c,d) = 
     //multiply numerators
     let temp1 = (a*d) + (b*c)
     //multiply denominators
@@ -26,6 +26,56 @@ let (.+) (a,b)(c,d) =
         else GCD(i,j-i)
     // finction call to find the greates common divisor
     let temp3 = GCD(temp1,temp2)
-    printfn "the answer is %f" (temp1/temp3),(temp2/temp3);;
+    printfn "the answer is %i" (temp1/temp3),(temp2/temp3);;
 //let answer = (1,2) (.+) (2,3)
-let answer = (.+)(1.0,2.0)(2.0,3.0);;
+let answer = (.+)(1,2)(2,3);;
+printfn "this is the newest list %A" answer *)
+
+// problem 17
+
+// function to reverse a list of lists remember you cant .rev an empty list
+let revList xs = List.map (fun x -> List.rev x) xs;;
+
+  let reverseList() =
+
+    let myList = [[0;1;1];[3;2];[];[5]]
+    printfn "Original list is %A" myList;;
+
+    let reverseOrder = revList [[0;1;1];[3;2];[];[5]]
+    printfn "Reverse of that list is %A" reverseOrder;;
+
+  reverseList();; 
+
+// problem 18
+
+// interleaving sub list
+let rec interleave = function   
+    |([], ys) -> ys
+    |(xs, []) -> xs
+    |(x::xs, y::ys) -> x :: y :: interleave (xs,ys)
+    
+//printfn "this is the interleave %A" 
+let interList = interleave ([1;2;3],[4;5;6]) 
+printfn "interleave list %A" interList 
+
+// problem 19
+
+// cutting even list in half
+// Function to cut a list where the first list is size n.
+let gencut (n, list) = 
+    let rec aux = function
+        | 0, xs, ys -> (List.rev xs,ys) //must return xs reversed since you are adding the head of ys to the beginning of the list each time.
+        | n, xs, [] -> (xs, [])
+        | n, xs, ys -> aux(n-1, List.head ys :: xs, List.tail ys)
+    aux (n, [], list)
+let cut list = 
+    let n = (List.length list)/2
+    ///gencut(n,list)
+    let newList = gencut(n,list)
+    printfn "the new list is %A" newList;; 
+
+    //////////////////////////////////
+    // second attempt
+    //let reverseOrder = revList [[0;1;1];[3;2];[];[5]]
+   // let gencutdata = ([1;2;3],[4;5;6])
+    
---
+++ ProblemSet1.1/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// problem 20 
+// this next problem is shuffling to get back to the original number
+
+let perfectShuffle xs =
+ let h = (List.length xs) / 2
+ xs
+ |> List.mapi (fun i x->(if i<h then i * 2 else ((i-h) * 2) + 1), x)
+ |> List.sortBy fst
+ |> List.map snd
+
+let orderCount n =
+ let xs = [1..n]
+ let rec spin count ys =
+   if xs=ys then count
+   else ys |> perfectShuffle |> spin (count + 1)
+ xs |> perfectShuffle |> spin 1
+
+[ 8; 24; 52] |> List.iter (fun n->n |> orderCount |> printfn "input : %d ----> output %d" n)
+
+// problem 21 uncurried function with cartesian product
+let rec cartesian = function
+    | (xs, []) -> []
+    | (xs, y::ys) -> List.map (fun x -> (x, y)) xs @ cartesian (xs, ys) ;;
+printfn " the cartesian product is %A" cartesian(["a"; "b"; "c"] [1; 2])
