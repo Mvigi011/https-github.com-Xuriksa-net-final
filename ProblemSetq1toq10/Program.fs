c1f9ed693207180d00eb1fa03f93137d142fdd1b
Michael V's PC <Michael V's PC@LAPTOP-G17J4HIR>
2020-01-27 15:16:48 -0500

    These are the basics of probelms 1-10 of the problem set, a couple are unanswered because I guess I had trouble understanding the syntax or the question

---
+++ ProblemSetq1toq10/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let x = 4.0 + 5.6
+printfn "%f" x
+
+
+// problem 2
+let add1Param x y = (+) 5 3
+
+printfn "this is the number %A" add1Param
+
+//problem 6
+let  listX = [1;2;3;4]
+let  listY =  5::6::7::[]
+let listXY = List.map(List.head(listX @ listY))
+//printfn "this is the concatination %A" List.map(List.head(listX @ listY))
+
+//problem 7
+
+//problem 8 
+let rec foo = function
+| (xs, [])    -> xs
+| (xs, y::ys) -> foo (xs@[y], ys) //compiles
+
+// problem 9
+let y = [fun x -> x+1] // no 
+let z = fun x -> x::[5]// int -> int list
+
+// problem 10
+let a1 = fun x y -> x+y+"." // string > string > string
+
+// problem 11
+let a2 = fun xs -> List.map (+) xs //xs:int list -> (int -> int) list
+
+
